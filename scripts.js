a96a59e1e112238d89be4033708633299ceda5ce
Anrean-L <anrean_augusta@vk.com>
2024-05-03 17:29:57 +0500

    Solved problem with negative numbers

--- scripts.js
+++ scripts.js
@@ -10,15 +10,16 @@ function factorial(n) {
 
 function calculate(e) {
     const number = parseInt(e.target.value, 10);
-    if (!number) return;
+    if (!number && number !== 0) return;
     const result = document.querySelector('.caluclating__result');
     if (number > 27) {
-        result.innerHTML = 'Число слишком большое!'
+        result.innerHTML = 'Число слишком большое!';
+    } else if (number <= 0) {
+        result.innerHTML = 'n должно быть натуральным';
     } else {
         const output = Math.round(factorial(number * 2) / factorial(number + 1) / factorial(number));
         result.innerHTML = `C<span class="index">${number}</span> = ${output}`;
     }
 }
-
 document.querySelector('.calculating__input').addEventListener('input', calculate);
 document.querySelector('.examples__overlay').addEventListener('click', (e) => e.target.classList.add('examples__overlay--hidden'))
